7249e319294f27d4cde0263a5c8fb6debeaecc97
YOKI1216 <1151509140@qq.com>
2023-08-29 10:58:40 +0800

    添加Ble一些基本类，下一步迁移模块进入项目，进一步完善蓝牙模块功能

--- app/src/main/java/com/hanamilink/MainActivity.java
+++ app/src/main/java/com/hanamilink/MainActivity.java
@@ -2,6 +2,7 @@ package com.hanamilink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
