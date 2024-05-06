e7d36c56a7a8b51a1867aed1a3e43ad6664baa98
YOKI1216 <1151509140@qq.com>
2024-05-06 16:41:43 +0800

    feat: 优化底部导航栏，删除底部导航栏activity

--- app/src/main/java/com/hanamilink/MainActivity.java
+++ app/src/main/java/com/hanamilink/MainActivity.java
@@ -2,54 +2,16 @@ package com.hanamilink;
 
 
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.Manifest;
-import android.bluetooth.BluetoothDevice;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.hanamiLink.ble.BLEDeviceManager;
-import com.hanamiLink.ble.BleDevice;
-import com.hanamiLink.utils.PermissionUtils;
-import com.hanamiLink.utils.StatusBarUtil;
-import com.hanamiLink.utils.ToastUtil;
-import com.hanamilink.activity.BleManagerActivity;
-import com.hanamilink.activity.BottomNavigationActivity;
-import com.hanamilink.ble.BleManagerAdapter;
-import com.hanamilink.bluetooth.adapter.DeviceAdapter;
 import com.hanamilink.databinding.ActivityBottomNavigationBinding;
-
-import org.greenrobot.eventbus.EventBus;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import pub.devrel.easypermissions.AppSettingsDialog;
-import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/hanamilink/activity/ui/notifications/BleManagerFragment.java
+++ app/src/main/java/com/hanamilink/activity/ui/notifications/BleManagerFragment.java
@@ -21,16 +21,26 @@ public class BleManagerFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        // 创建 BleManagerViewModel 实例来处理 BLE 设备通信逻辑
         BleManagerViewModel bleManagerViewModel =
                 new ViewModelProvider(this).get(BleManagerViewModel.class);
 
+        // 使用 Data Binding 将 Fragment 的布局文件与视图关联起来
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+
+        // 获取与 FragmentNotificationsBinding 关联的根视图
         View root = binding.getRoot();
 
+        // 从 FragmentNotificationsBinding 获取名为 textNotifications 的 TextView
         final TextView textView = binding.textNotifications;
+
+        // 观察 BleManagerViewModel 中的文本数据变化，并更新到 textNotifications TextView
         bleManagerViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        // 返回根视图作为 Fragment 的视图
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
