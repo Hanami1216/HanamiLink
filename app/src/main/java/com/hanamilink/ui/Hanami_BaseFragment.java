fa9a3f227f364b7fb2aad6f5d347e3fefa94b668
YOKI1216 <1151509140@qq.com>
2024-05-09 11:52:10 +0800

    refactor: 主页切换成音乐均衡器，与DashboardFragment同时对比修改

--- app/src/main/java/com/hanamilink/ui/Hanami_BaseFragment.java
+++ app/src/main/java/com/hanamilink/ui/Hanami_BaseFragment.java
@@ -58,23 +58,31 @@ public class Hanami_BaseFragment extends Fragment {
         this.bundle = bundle;
     }
 
+    /**
+     * 在给定的容器中切换 Fragment
+     *
+     * @param containerId   用于容纳新 Fragment 的布局容器的 ID
+     * @param fragment      要切换到的新 Fragment
+     * @param fragmentTag   将要分配给新 Fragment 的标记，可以为null
+     */
     public void changeFragment(int containerId, Fragment fragment, String fragmentTag) {
+        // 确保 fragment 不为 null，当前 Fragment 已经添加到 activity 中，且当前 Fragment 未被分离
         if (fragment != null && this.isAdded() && !this.isDetached()) {
+            // 获取当前 Fragment 的 ChildFragmentManager，并使用它启动一个新的 FragmentTransaction
             FragmentManager fragmentManager = this.getChildFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            // 根据是否存在 fragmentTag，使用 FragmentTransaction 的 replace 方法来替换容器中的 Fragment
             if (!TextUtils.isEmpty(fragmentTag)) {
                 fragmentTransaction.replace(containerId, fragment, fragmentTag);
             } else {
                 fragmentTransaction.replace(containerId, fragment);
             }
 
-            fragmentTransaction.addToBackStack((String)null);
+            // 将事务添加到后退栈
+            fragmentTransaction.addToBackStack(null);
+            // 提交事务进行切换，并允许状态丢失
             fragmentTransaction.commitAllowingStateLoss();
         }
-
-    }
-
-    public void changeFragment(int containerId, Fragment fragment) {
-        this.changeFragment(containerId, fragment, (String)null);
     }
 }
